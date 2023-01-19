94a3be0822114305530c422c79ef85e9077a1244
zanehirning <a02371568@usu.edu>
2023-01-18 22:11:43 -0700

    Creating/checking room

--- chat/views.py
+++ chat/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
-
+from .models import Room, Message
+from django.shortcuts import redirect
 # Create your views here.
 def home(request):
     return render(request, 'chat/home.html')
+
+def room(request, room):
+    return render(request, 'chat/room.html')
+
+def checkview(request):
+    room = request.POST['room_name']
+    username = request.POST['username']
+    if Room.objects.filter(name=room).exists():
+        return redirect('/' + room + '/?username=' + username)
+    else:
+        new_room = Room.objects.create(name=room)
+        new_room.save()
+        return redirect('/' + room + '/?username=' + username)
